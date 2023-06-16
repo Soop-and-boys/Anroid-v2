71a2aad2b12bc56417522fc00486bd4ba92c86bb
realhsb <realhsb@gmail.com>
2023-06-16 19:00:46 +0900

    chore : 시흥 봉사활동 데이터 추가

---
+++ app/src/main/java/com/soop/moblieprogram/home/Data_siheung.kt
@@ -0,0 +1,15 @@
+package com.soop.moblieprogram
+
+class Data_siheung {
+    companion object {
+        val imageList = arrayListOf(
+            "https://sh1365.or.kr/upload/editordata/가족캠핑.jpg",
+            "https://sh1365.or.kr/upload/editordata/6월_봉봉의날_홍보리플릿.png",
+            "https://sh1365.or.kr/upload/editordata/청소년_퍼실리테이션_교육과_활동_모집_홍보리플릿0.png",
+            "https://sh1365.or.kr/upload/editordata/제목없음_10.jpg",
+            "https://sh1365.or.kr/upload/editordata/2023년_시흥시장_표창_대상자_추천_안내.png",
+            "https://sh1365.or.kr/upload/editordata/KakaoTalk_20230516_145740912_1.jpg"
+        )
+    }
+}
+
