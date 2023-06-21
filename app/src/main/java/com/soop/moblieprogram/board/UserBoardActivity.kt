e1f2f54947915d0e0cef58f38cd1a2966a54e559
Jung Yunho <a01049048063@gmail.com>
2023-06-21 11:06:50 +0900

    feat: 게시판에서 채팅창 이동

--- app/src/main/java/com/soop/moblieprogram/board/UserBoardActivity.kt
+++ app/src/main/java/com/soop/moblieprogram/board/UserBoardActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.soop.moblieprogram.ChatRoomActivity
 import com.soop.moblieprogram.databinding.ActivityUserBoardBinding
 
 
@@ -20,6 +21,14 @@ class UserBoardActivity : AppCompatActivity() {
 
         binding.contactBtn.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://open.kakao.com/o/gt3bFcqf"))
+            startActivity(intent)
+        }
+        binding.createChatBtn.setOnClickListener {
+            val intent = Intent(this, ChatRoomActivity::class.java)
+            val roomId = "asd"
+            intent.putExtra("roomId", roomId) // 채팅방 ID를 전달
+            val roomTitle ="사람들아 모여라"
+            intent.putExtra("roomTitle", roomTitle) // 채팅방 제목을 전달
             startActivity(intent)
         }
     }
